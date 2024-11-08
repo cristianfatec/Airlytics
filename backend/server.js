97f136bdacd43528dc63e5db452369477fd7936c
Pedro Moreira <pedrogonmoreira@gmail.com>
2024-11-07 21:18:59 -0300

    ajustes rota padrao /

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');  // Importa dotenv
+const dotenv = require('dotenv'); // Importa dotenv
 
-dotenv.config();  // Carrega as variáveis de ambiente do .env
+dotenv.config(); // Carrega as variáveis de ambiente do .env
 
 const readingsRouter = require('./routes/routes'); // Ajuste o caminho conforme necessário
 const userRoutes = require('./routes/userRoutes'); // Importa as rotas de usuário
@@ -12,16 +12,19 @@ const app = express();
 const mongoDB = process.env.MONGODB_URI; // Carregando a URI do .env
 
 // Conectar ao MongoDB
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conexão com o MongoDB estabelecida!'))
-    .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexão com o MongoDB estabelecida!'))
+  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 app.use(bodyParser.json());
-app.use('/api/readings', readingsRouter); // Prefixo das rotas
-app.use('/api/users', userRoutes); // Prefixo das rotas de usuário
+app.use('/readings', readingsRouter); // Prefixo das rotas
+app.use('/users', userRoutes); // Prefixo das rotas de usuário
+app.get('/', (req, res) => {
+  res.send('success');
+});
 
 const porto = 5000; // Altere a porta para 5000
 app.listen(porto, () => {
-    console.log('Servidor em execução no porto: ' + porto);
+  console.log('Servidor em execução no porto: ' + porto);
 });
-
